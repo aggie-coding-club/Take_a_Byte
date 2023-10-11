0850e6034c6364be9f785978a1a2b4bf53c0a9a8
Samffprice <145379682+Samffprice@users.noreply.github.com>
2023-10-11 17:37:40 -0500

    Add files via upload
    test to get food data from USDA FoodData central.

---
+++ foodapi.py
@@ -0,0 +1,25 @@
+#access the USDA food database, for Take a Byte
+#
+
+import os
+os.system('pip3 install requests')
+import requests
+import json 
+import os
+
+type = '/v1/foods/search'
+key = 'DEMO_KEY'
+#url: https://api.nal.usda.gov/fdc/v1/foods/search/?api_key=
+
+params = {
+    key : 'DEMO_KEY',
+    'query' : 'cheese',
+    'pageSize' : 1,
+    'pageNumber' : 1,
+    
+}
+
+fetch = requests.get(f"https://api.nal.usda.gov/fdc{type}/?api_key={params[key]}&query={params['query']}")
+response = fetch.json() 
+print(response)
+#this does not format well rn, just a mess of info
