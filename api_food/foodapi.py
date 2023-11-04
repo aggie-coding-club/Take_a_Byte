d0f0798d4758efd09c3b2a9fbc367b1de3b93d27
Samffprice <145379682+Samffprice@users.noreply.github.com>
2023-11-04 15:41:27 -0500

    Update foodapi.py
    Fixing errors in code. LMK if I missed anything.

--- api_food/foodapi.py
+++ api_food/foodapi.py
@@ -1,44 +1,44 @@
 #access the USDA food database, for Take a Byte
 #
-#testing change
 
-import requests #pip3 install requests
-import json 
+import json
+import requests  # pip3 install requests
 
-type = '/v1/foods/search'
-key = '8Wd9vaf4eJdizRTosIiIDPce3jOOloEfQc9aiDM1'
-#url: https://api.nal.usda.gov/fdc/v1/foods/search/?api_key=
+fdc_search = '/v1/foods/search'
+api_key = '8Wd9vaf4eJdizRTosIiIDPce3jOOloEfQc9aiDM1'
+# url: https://api.nal.usda.gov/fdc/v1/foods/search/?api_key=
 
 params = {
-    'key' : key,
-    'query' : 'cheese',
-    'pageSize' : 5,
-    'pageNumber' : 1,
-    'dataType' : 'Branded',
-    'numberOfResultsPerPage' : 5,
-    
+    'key': api_key,
+    'query': 'cheese',
+    'pageSize': 5,
+    'pageNumber': 1,
+    'dataType': 'Branded',
+    'numberOfResultsPerPage': 5,
+
 }
 
-fetch = requests.get(f"https://api.nal.usda.gov/fdc{type}/?api_key={params['key']}&query={params['query']}&dataType={params['dataType']}&pageSize={params['pageSize']}&pageNumber={params['pageNumber']}&numberOfResultsPerPage={params['numberOfResultsPerPage']}")
-response = fetch.json() 
+fetch = requests.get(
+    f"https://api.nal.usda.gov/fdc{fdc_search}/?api_key={params['key']}&query={params['query']}&dataType={params['dataType']}&pageSize={params['pageSize']}&pageNumber={params['pageNumber']}&numberOfResultsPerPage={params['numberOfResultsPerPage']}", timeout=10)
+response = fetch.json()
 json_formatted_str = json.dumps(response, indent=2)
 
 #saves response in a file (to mess around with formatting/parsing)
 path = 'api_food/api_food_response/'
-_file = open(f'{path}response_{params["query"]}', 'w')
+_file = open(f'{path}response_{params["query"]}', 'w', encoding="utf-8")
 _file.write(json_formatted_str)
 _file.close()
 
-#trying to isolate specific info
-food_brand = [] # {food description : food brand}
+# trying to isolate specific info
+food_brand = []  # {food description : food brand}
 for food in response.get('foods'):
     try:
-         brand = food['brandName']
-    except:
-         brand = 'No brand'
+        brand = food['brandName']
+    except KeyError:
+        brand = 'No brand'
     desc = food["description"]
-    food_brand.append({desc : brand})
+    food_brand.append({desc: brand})
 
 
 print(food_brand)
-#print(json_formatted_str) returns too much info rn.
+
